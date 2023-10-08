795c2ffffbdae2cd00a4f6f82bb75b098154006a
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-10-08 16:40:13 +0530

    Add files via upload

---
+++ Basic concept of Python.py
@@ -0,0 +1,568 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Basic concept of Python
+# 
+
+# ### Video 4, 5, 6, 7, 8, 9 , 10
+
+# # Video 4 ( Hello world)
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+1+4
+
+
+# In[3]:
+
+
+5-3
+
+
+# In[4]:
+
+
+3-8
+
+
+# In[5]:
+
+
+2*2
+
+
+# In[6]:
+
+
+2*2*2
+
+
+# In[7]:
+
+
+2**3
+
+
+# In[8]:
+
+
+9/3
+
+
+# In[9]:
+
+
+10/3
+
+
+# In[10]:
+
+
+10//3
+
+
+# In[11]:
+
+
+10%3
+
+
+#  BODMAS RULE
+
+# In[12]:
+
+
+2*60+30
+
+
+# In[13]:
+
+
+(30+2)*60
+
+
+# # Video 5 ( mathematical operation)
+
+# In[14]:
+
+
+
+# 10 & 20 ===> opreands 
+# + is known as operator
+# both together form an operation
+
+10+20
+
+
+# In[15]:
+
+
+2**2
+
+
+# In[16]:
+
+
+# square root
+# ** is a power operator
+49**(1/2)
+
+
+# In[17]:
+
+
+2.0 * 3
+
+
+# In[18]:
+
+
+9/3
+
+
+# In[19]:
+
+
+10/3
+
+
+# In[20]:
+
+
+10//3
+
+
+# In[21]:
+
+
+10/0
+
+
+# In[ ]:
+
+
+8/ 1-1
+
+
+# In[ ]:
+
+
+782.434%43
+
+
+# In[ ]:
+
+
+87 %(8//7)**(1/4)
+
+
+# # Video 6 ( Strings)
+
+# In[ ]:
+
+
+# String
+"Hello world"
+
+
+# In[ ]:
+
+
+hello world
+# if not writing without quote it will show error
+
+
+# In[ ]:
+
+
+print("hello world")
+
+
+# In[ ]:
+
+
+# print python 's world 
+# with five method
+
+
+# In[ ]:
+
+
+# method 1
+print("Python's world")
+
+
+# In[ ]:
+
+
+# Method 2
+print("""Python's world""")
+
+
+# In[ ]:
+
+
+# method 3
+
+print('Python\'s world')
+
+
+# In[ ]:
+
+
+# method 4
+print("Python\'s world'")
+
+
+# In[ ]:
+
+
+# method 5 
+print("Python's " 'world')
+
+
+# In[ ]:
+
+
+print("Python's \"world\"")
+
+
+# In[ ]:
+
+
+print('Python\'s"World"')
+
+
+# # Video 7 (input function)
+
+# In[ ]:
+
+
+input('Are you an alien ?')
+
+
+# In[ ]:
+
+
+input('Which is your favourite team ?')
+
+
+# In[ ]:
+
+
+print('Hey there!\n thank you')
+
+
+# In[ ]:
+
+
+print('Hey there \nthank you')
+
+
+# In[ ]:
+
+
+print('carrots\t Rs.30')
+
+
+# In[51]:
+
+
+print("Item \tQty \nApples \t5 \nPlums \t10")
+
+
+# # video 8 ( String operations)
+
+# In[28]:
+
+
+"Python"
+
+
+# In[29]:
+
+
+"1"
+
+
+# In[31]:
+
+
+type("1")
+
+
+# In[33]:
+
+
+"HEllo" + "World"
+# concate
+
+
+# In[34]:
+
+
+"Hello " +" "+"World"
+
+
+# In[35]:
+
+
+1+2
+
+
+# In[37]:
+
+
+"1"+"2"
+
+
+# In[38]:
+
+
+"2"+"5"
+
+
+# In[40]:
+
+
+"1"+2
+# only str can be added to str 
+#  and only integer can be added to str
+
+
+# In[43]:
+
+
+"python "*3
+# only integer can multiply the str.
+# str can not be sub or divide
+
+
+# In[54]:
+
+
+print(("I am a polite person.\n")*3)
+
+
+# In[55]:
+
+
+# \n does not work when print function is not used.
+
+
+# ## Video 9 ( VARIABLES)
+
+# VARIABLE reserved memory location
+
+# In[59]:
+
+
+a=1
+# = is a assign operator
+
+
+# In[58]:
+
+
+a
+
+
+# In[60]:
+
+
+b=2
+
+
+# In[61]:
+
+
+b
+
+
+# In[62]:
+
+
+c=3
+
+
+# In[65]:
+
+
+d=a+b+c
+
+
+# In[66]:
+
+
+d
+
+
+# In[67]:
+
+
+del d
+
+
+# In[69]:
+
+
+d
+# this must show error
+
+
+# In[70]:
+
+
+x=5
+print(x)
+
+
+# In[71]:
+
+
+x=3
+print(x)
+
+
+# In[73]:
+
+
+print(x)
+# you will always get the value which is assign latest into a variable.
+
+
+# In[74]:
+
+
+y=1
+z=x*y
+z
+
+
+# In[75]:
+
+
+a="python"
+
+
+# In[76]:
+
+
+a
+
+
+# In[77]:
+
+
+a=1
+print(a)
+
+
+# In[78]:
+
+
+_a=1
+print(_a)
+
+
+# In[80]:
+
+
+# number cannot be used as a variable.
+# python is case sensitive 
+
+
+# In[81]:
+
+
+age=1
+Age =2
+
+
+# In[82]:
+
+
+print(age)
+print(Age)
+
+
+# In[83]:
+
+
+a1='#"Hello Wrold"'
+print(a1)
+
+
+# # Video 10 (increment)
+
+# In[85]:
+
+
+a=1
+a=a+1
+print(a)
+
+
+# In[87]:
+
+
+b=2
+b+=1
+# this method is mostly prefered
+print(b)
+
+
+# In[89]:
+
+
+x=5
+x-=2
+print(x)
+
+
+# In[90]:
+
+
+c=10
+c*=2
+print(c)
+
+
+# In[91]:
+
+
+d=3
+d/=2
+print(d)
+
+
+# In[93]:
+
+
+p="python"
+p+=1
+print(p)
+# as we know we cannot add str and integer
+
+
+# In[96]:
+
+
+z="thank you"
+z+=" 1"
+print(z)
+# we can add it by using it but it will lose it integer property
+
+
+# In[ ]:
+
+
+
+
